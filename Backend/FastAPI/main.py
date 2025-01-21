0c91a800a0a5f0667db6bda1d76d3e5954aa2e9a
Jesús Bermudez Zamora <99784430+JesusZamora20@users.noreply.github.com>
2025-01-21 16:33:23 -0500

    Update main.py -> FastAPI hello world

--- Backend/FastAPI/main.py
+++ Backend/FastAPI/main.py
@@ -1,3 +1,23 @@
 from fastapi import FastAPI
 
 app = FastAPI()
+
+@app.get("/")
+async def root():
+  return "Hi FastAPI"
+
+
+@app.get("/url")
+async def urk():
+  return{"url" : "https://python.org"}
+  
+#Iniciar el server -> uvicorn main:app --reload
+#Detener el server: ctrl + c
+
+#documentacion con swagger -> localhost/docs
+#documentacion con redocly -> localhost/redoc
+
+#operacion get
+#Download postman <- delete when downloaded
+
+
