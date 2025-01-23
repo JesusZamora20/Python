29fe095996cf565a3465e354af37a1ae6418c345
Jesús Bermudez Zamora <99784430+JesusZamora20@users.noreply.github.com>
2025-01-23 16:42:21 -0500

    Update transcript.py -> Operation POST

--- transcript.py
+++ transcript.py
@@ -1,7 +1,11 @@
+#post put delete
 
-
-
-
-
+@app.post(/user/)
+async def user(user: User):
+  if type(search_user(user.id)) == User:
+    return {"Error": : "User already exists"}
+  else:
+      users_list.append(user)
+  
 
 
