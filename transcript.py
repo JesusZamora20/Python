d34e0d9d0d288c123acec3d51fd2b3f4c250e92c
Jesús Bermudez Zamora <99784430+JesusZamora20@users.noreply.github.com>
2025-01-24 16:46:52 -0500

    Update transcript.py -> OAuth2 part 1

--- transcript.py
+++ transcript.py
@@ -1,4 +1,33 @@
-
+#OAuth2
   
+#new file -. basic_auth_users.py
+#run uvicorn basic_auth_users:app --reload
 
+from fastapi import FastAPI
+from pydantic import BaseModel
+app = FastAPI()
 
+class User(BaseModel):
+  username: str
+  full_name: str
+  email: str
+  disabled: bool  
+
+users_db = {
+  "jesus": {
+    "username": "jesusinho",
+    "full_name": "jesus Do Santos",
+    "email": "jesusdossantos@gmail.com",
+    "disabled": False,
+    "password": "123456"
+  },
+  "Yolanda": {
+    "username": "Yolandayro",
+    "full_name": "Yolanda Isabel",
+    "email": "Yolandita@gmail.com",
+    "disabled": True,
+    "password": "56789"
+  },
+}
+
+#4:12
