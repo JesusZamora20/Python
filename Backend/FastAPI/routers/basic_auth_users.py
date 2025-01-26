61d841a5973c62f3cdeddd12bf1da19177e11c84
JESUS DAVID BERMUDEZ ZAMORA <jbermudezamora87@gmail.com>
2025-01-26 00:03:56 -0500

    OAuth2 part 3

--- Backend/FastAPI/routers/basic_auth_users.py
+++ Backend/FastAPI/routers/basic_auth_users.py
@@ -34,10 +34,16 @@ users_db = {
   }
 }
 
-def search_user(username: str):
+
+def search_user_db(username: str):
   if username in users_db:
     return UserDB(**users_db[username])
 
+
+def search_user(username: str):
+  if username in users_db:
+    return User(**users_db[username])
+  
 
 async def current_user(token: str = Depends(oauth2)):
   user = search_user(token)
@@ -46,6 +52,11 @@ async def current_user(token: str = Depends(oauth2)):
       status_code=status.HTTP_401_UNAUTHORIZED,
       detail="Credenciales de autenticación inválidas",
       headers={"WWW-Authenticate": "Bearer"})
+  
+  if user.disabled:
+    raise HTTPException(
+      status_code=status.HTTP_400_BAD_REQUEST,
+      detail="Usuario deshabilitado")
 
   return user
 
@@ -58,7 +69,7 @@ async def login(form: OAuth2PasswordRequestForm = Depends()):
       status_code=status.HTTP_400_BAD_REQUEST, 
       detail="El usuario no es correcto")
 
-  user = search_user(form.username)
+  user = search_user_db(form.username)
   if not form.password == user.password:
     raise HTTPException(
       status_code=status.HTTP_400_BAD_REQUEST, 
