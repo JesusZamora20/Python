fcfadb414789353a5f9882596bf855ef8a8fbd39
Jesús Bermudez Zamora <99784430+JesusZamora20@users.noreply.github.com>
2025-01-28 15:52:11 -0500

    JWT auth, final

--- Backend/FastAPI/routers/jwt_auth_users.py
+++ Backend/FastAPI/routers/jwt_auth_users.py
@@ -1,15 +1,19 @@
 from fastapi import FastAPI, Depends, HTTPException, status
 from pydantic import BaseModel
 from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
-from jose import jwt
+from jose import jwt, JWTError
 from passlib.context import CryptContext
 from datetime import datetime, timedelta
 
+# install python-jose
+#pip install "python-jose[cryptography]"
+#pip install "passlib[bcrypt]"
+#5:38:32
+
 #--------------------------------------------
 ALGORITHM = "HS256"
-
 ACCESS_TOKEN_DURATION = 1
-
+SECRET = "15f85a18b08c9fc377d83e44f9a39688640dbdf0f7f452b39126f43bbd4018f5"
 app = FastAPI()
 
 oauth2 = OAuth2PasswordBearer(tokenUrl="login")
@@ -45,13 +49,44 @@ users_db = {
   }
 }
 
-# install python-jose
-#pip install "python-jose[cryptography]"
-#pip install "passlib[bcrypt]"
+
 
 def search_user_db(username: str):
   if username in users_db:
     return UserDB(**users_db[username])
+
+def search_user(username: str):
+  if username in users_db:
+    return User(**users_db[username])
+
+
+async def auth_user(token: str = Depends(oauth2)):
+  exception = HTTPException(
+      status_code=status.HTTP_401_UNAUTHORIZED,
+      detail="Credenciales de autenticación inválidas",
+      headers={"WWW-Authenticate": "Bearer"})  
+
+  try:
+    username = jwt.decode(token, SECRET, algorithms=[ALGORITHM]).get("sub")
+    if username is None:
+      raise exception
+
+  except JWTError:
+    raise exception
+
+  return search_user(username)
+
+
+
+
+async def current_user(user: User = Depends(auth_user)):
+  
+  if user.disabled:
+    raise HTTPException(
+      status_code=status.HTTP_400_BAD_REQUEST,
+      detail="Usuario deshabilitado")
+
+  return user
 
 
 @app.post("/login")
@@ -68,17 +103,20 @@ async def login(form: OAuth2PasswordRequestForm = Depends()):
   if not crypt.verify(form.password, user.password):
     raise HTTPException(
       status_code=status.HTTP_400_BAD_REQUEST, 
-      detail="La contraseña no es correcta")
+      detail="La password no es correcta")
     
-  expire = datetime.utcnow() +timedelta(minutes=ACCESS_TOKEN_DURATION)
 
+  access_token = {
+    "sub": user.username,
+    "exp": datetime.utcnow() +timedelta(minutes=ACCESS_TOKEN_DURATION)
+  }
   
-  
-  return {"access_token": user.username, "token_type": "bearer"}
+  return {"access_token": jwt.encode(access_token, SECRET, algorithm=ALGORITHM), "token_type": "bearer"}
 
-
-
-
+ 
+@app.get("/users/me")
+async def me(user: User = Depends(current_user)):
+  return user
 
 
 
