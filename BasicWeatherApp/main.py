69d45c898054f4c762d43850d5535e7c01323811
Jesús Bermudez Zamora <99784430+JesusZamora20@users.noreply.github.com>
2025-02-04 15:09:27 -0500

    Getting openweather API key

--- BasicWeatherApp/main.py
+++ BasicWeatherApp/main.py
@@ -12,7 +12,14 @@
 #pip install requests python-dotenv Flask
 #pip freeze > requirements.txt
 
+#_______________________
 # create a .gitignore file and write:
 # .venv
 # .env
 #create .env file
+
+#_______________________
+#GET OPENWEATHERMAP KEY
+# in .env file -> 
+# API_KEY=ac4a10a6f2c41f4f8657aebff02b9e3b
+
