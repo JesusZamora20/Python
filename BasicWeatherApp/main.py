2408a78f98a62a73f629c1b21659a14dff14645e
Jesús Bermudez Zamora <99784430+JesusZamora20@users.noreply.github.com>
2025-02-04 15:03:17 -0500

    Create Project on remote

---
+++ BasicWeatherApp/main.py
@@ -0,0 +1,18 @@
+# virtual environment 
+
+# py -m venv .venv
+# source .venv/Scripts/activate
+# deactivate
+
+#______________________
+#miniproject with Flask 
+
+# py -m venv .venv
+# source .venv/Scripts/activate
+#pip install requests python-dotenv Flask
+#pip freeze > requirements.txt
+
+# create a .gitignore file and write:
+# .venv
+# .env
+#create .env file
