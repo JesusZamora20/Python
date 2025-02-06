ce2ead7b94d8465d747dbee0738e0be054471ff4
JESUS DAVID BERMUDEZ ZAMORA <jbermudezamora87@gmail.com>
2025-02-05 21:59:28 -0500

    Basic Weather app With Flask, final

--- BasicWeatherApp/weather.py
+++ BasicWeatherApp/weather.py
@@ -9,12 +9,16 @@ def get_current_weather(city="Bogota"):
   request_url = f'https://api.openweathermap.org/data/2.5/weather?appid={os.getenv("API_KEY")}&q={city}&units=imperial'
 
   weather_data = requests.get(request_url).json()
-  return weather-data
+  return weather_data
 
 if __name__ == "__main__":
   print("\n *** Get Current Weather Conditions *** \n")
 
   city = input("\n please enter a city name: ")
+
+  #check for empty string or string with only spaces
+  if not bool(city.strip()):
+    city= "Madrid"
 
   weather_data = get_current_weather(city)
 
