cc9ac37bc0c48a570a93a7d7c18b401f163cecf5
JESUS DAVID BERMUDEZ ZAMORA <jbermudezamora87@gmail.com>
2025-02-13 01:05:29 -0500

    writing more views

--- DjangoPollApp/polls/views.py
+++ DjangoPollApp/polls/views.py
@@ -3,3 +3,13 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("Hello, world. This is polls index")
+
+def detail(request, question_id):
+    return HttpResponse("You're looking at question %s." % question_id )
+
+def results(request, quesiton_id):
+    response = "You're looking at the results of question %s."
+    return HttpResponse(response % quesiton_id)
+
+def vote(request, question_id):
+    return HttpResponse("You're voting on question %s." % question_id)
