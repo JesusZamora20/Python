492a633e56c2f8a2e78c73a3bb67874127a6565f
jesu <jbermudezamora87@gmail.com>
2025-04-03 23:34:52 -0500

    objects and class part 2

--- OOP/classandobject.py
+++ OOP/classandobject.py
@@ -7,3 +7,30 @@ class Auto:
 
 taxi = Auto()
 print(taxi.model)
+
+#----------------------
+class Jugadores:
+    j1 = "Messi"
+    j2 = "Cristiano"
+
+class Jugadores2:
+    j1 = "James"
+    j2 = "Bale"
+
+#----------------------
+
+class Nombre:
+    pass
+
+jesus = Nombre()
+yola = Nombre()
+
+jesus.edad = 20
+jesus.sexo = "Masculino"
+jesus.pais = "Colombia"
+
+yola.edad = 19
+yola.sexo = "Femenino"  
+yola.pais = "venezuela"
+
+print(jesus.edad, jesus.sexo, jesus.pais)
